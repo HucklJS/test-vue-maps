fb8090cf56f107321ad812c6bc17cc0768485108
Vasiliy <vasilij.1357@yandex.ru>
2020-11-23 16:47:03 +0300

    Now we can see nearest point to our click coords

--- src/App.vue
+++ src/App.vue
@@ -14,22 +14,30 @@
         marker-id="clicked-marker"
         hint-content="clicked-marker"
       />
+
+      <ymap-marker
+        v-if="closestPointCoords"
+        :coords="closestPointCoords"
+        marker-id="closest-point-coords"
+        hint-content="closest-point-coords"
+      />
     </yandex-map>
   </div>
 </template>
 
 <script>
-import {yandexMap, ymapMarker, loadYmap} from 'vue-yandex-maps'
-import InfoBar from "./components/InfoBar.vue"
-import MKADCoords from "./polygons/MKADCoords"
+  import {loadYmap, yandexMap, ymapMarker} from 'vue-yandex-maps'
+  import InfoBar from "./components/InfoBar.vue"
+  import MKADCoords from "./polygons/MKADCoords"
 
-let ymaps
+  let ymaps
 
 export default {
   name: 'App',
   data() {
     return {
       clickedCoords: null,
+      closestPointCoords: null
     }
   },
   components: {
@@ -42,11 +50,28 @@ export default {
       await loadYmap();
       ymaps = window.ymaps; // здесь доступен весь функционал ymaps
 
-      const mkad = new ymaps.Polygon([MKADCoords]);
-      map.geoObjects.add(mkad)
+      this._paintMKADPolygon(map, MKADCoords)
     },
     onMapClick(mapE) {
       this.clickedCoords = mapE.get('coords')
+
+      this.closestPointCoords = this._findClosestPointTo(this.clickedCoords)
+    },
+
+    _paintMKADPolygon(map, coords) {
+      const mkad = new ymaps.Polygon([coords]);
+      map.geoObjects.add(mkad)
+    },
+    _findClosestPointTo: function ([clickY, clickX]) {
+      const distances = MKADCoords.map(
+        ([y, x]) => Math.sqrt((y - clickY) ** 2 + (x - clickX) ** 2)
+      )
+      const minDistance = Math.min(...distances)
+      const idOfMinDistance = distances.findIndex(
+        distance => distance === minDistance
+      )
+
+      return MKADCoords[idOfMinDistance]
     }
   }
   // other options
